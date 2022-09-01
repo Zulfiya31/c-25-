4b2618392cd2d622e8e0aea68a205359dd991ae4
Helen <elenaprofessor@yandex.ru>
2022-09-01 15:08:21 +0300

    Все задачи+1

--- gfg/gf1/задача 25/Program.cs
+++ gfg/gf1/задача 25/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Напишите цикл, 
+//который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+Console.WriteLine("Введите число  ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число  ");
+double b = double.Parse(Console.ReadLine());
+double i = 1;
+double sum = 0;
+while (i<=b) {
+   sum = Math.Pow(a,i);
+    i++;
+   
+}
+Console.WriteLine (a+","+b+"=>"+ sum);
