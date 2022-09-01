8738bce3f819100507159a796b69e1e526a81763
Helen <elenaprofessor@yandex.ru>
2022-09-01 15:05:29 +0300

    Все задачи

---
+++ gfg/gf1/массивы/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = {1,2,5,7,19,6,1,33};
+void PrintArray(int[]array)
+{
+    int count = array.Length;
+    for ( int i=0; i<count; i++){
+        Console.Write(array[i]+ ",");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
+// ВВеденные 8 чисел
